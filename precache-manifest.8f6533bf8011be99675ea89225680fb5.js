0e4b6deb2c26c54dceb8a9cdaafe5a3d2b1588d2
unknown <abdallah.gibreel@gmail.com>
2020-08-26 19:10:08 +0200

    Updates

---
+++ precache-manifest.8f6533bf8011be99675ea89225680fb5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf03bbc3cb9727c8ef4a0f90b72d46e6",
+    "url": "/crudapp/index.html"
+  },
+  {
+    "revision": "06c71bf3c817b355b3a2",
+    "url": "/crudapp/static/css/main.2c6133ef.chunk.css"
+  },
+  {
+    "revision": "63b9d90bba1b3746a1e0",
+    "url": "/crudapp/static/js/2.8f4bf464.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/crudapp/static/js/2.8f4bf464.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "06c71bf3c817b355b3a2",
+    "url": "/crudapp/static/js/main.3d068764.chunk.js"
+  },
+  {
+    "revision": "8d1326b45d6641caabf3",
+    "url": "/crudapp/static/js/runtime-main.23c4c990.js"
+  }
+]);
